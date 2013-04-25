6cd76d867db3e079c1c5cec6dd0628ffc7f70766
Bhavesh Patel <bhavesh.patel@insightsoftware.com>
2013-04-25 06:59:21 +0100

    Adding experimental folder and projects

---
+++ Experimental/ServiceStackClient/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using ServiceStack.ServiceClient.Web;
+
+namespace ServiceStackClient
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// The host ASP.NET MVC is working OK! When I visit this URL I see the metadata page.
+			const string BaseUrl = "http://localhost:1337/";
+
+			var restClient = new JsonServiceClient(BaseUrl);
+
+			restClient.SetCredentials("juser", "password");
+			restClient.AlwaysSendBasicAuthHeader = true;
+
+			Console.WriteLine("Enter name:");
+			string name = Console.ReadLine();
+			while (!string.IsNullOrEmpty(name)) {
+				ServiceStackService.Program.HelloResponse response = restClient.Get<ServiceStackService.Program.HelloResponse>("/hello/" + name);
+				//ServiceStackService.Program.HelloResponse response = restClient.Get<ServiceStackService.Program.HelloResponse>("/hello/bhavesh");
+				Console.WriteLine(response.Result);
+				name = Console.ReadLine();
+			}
+			
+		}
+	}
+}
---
+++ Experimental/ServiceStackService/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using ServiceStack.CacheAccess;
+using ServiceStack.CacheAccess.Providers;
+using ServiceStack.Configuration;
+using ServiceStack.ServiceInterface;
+using ServiceStack.ServiceInterface.Auth;
+using ServiceStack.WebHost.Endpoints;
+
+namespace ServiceStackService
+{
+	public class Program
+	{
+		[Authenticate]
+		public class Hello
+		{
+			public string Name { get; set; }
+		}
+
+		public class HelloResponse
+		{
+			public string Result { get; set; }
+		}
+
+		public class HelloService : Service
+		{
+			public object Any(Hello request)
+			{
+				HelloResponse result = new HelloResponse {Result = string.Format("Hello, {0}", request.Name)};
+				//IAuthSession authSession = this.GetSession();
+				//if (authSession != null) {
+				//	string displayName = authSession.DisplayName;
+				//	result.Result += string.Format("(or is it {0})", displayName);
+				//}
+				Console.WriteLine("Request: " + request.Name);
+				return result;
+			}
+
+			public object Post(Hello request)
+			{
+				object result = Any(request);
+				return result;
+			}
+		}
+
+		//Define the Web Services AppHost
+		public class AppHost : AppHostHttpListenerBase
+		{
+			private HelloService helloService;
+
+			public AppHost() : base("StarterTemplate HttpListener", typeof(HelloService).Assembly) { }
+
+			public HelloService HelloService
+			{
+				get { return helloService; }
+			}
+
+			public override void Configure(Funq.Container container)
+			{
+				Routes
+					.Add<Hello>("/hello")
+					.Add<Hello>("/hello/{Name}");
+				helloService = container.Resolve<HelloService>();
+
+				AuthFeature authFeature = new AuthFeature(() => new AuthUserSession(), new IAuthProvider[] {new BasicAuthProvider(), new TwitterAuthProvider(new AppSettings()),});
+				//authFeature.HtmlRedirect 
+				Plugins.Add(authFeature);
+				MemoryCacheClient memoryCacheClient = new MemoryCacheClient();
+				container.Register<ICacheClient>(memoryCacheClient);
+
+				var userRepository = new InMemoryAuthRepository();
+				container.Register<IUserAuthRepository>(userRepository);
+
+				string hash;
+				string salt;
+				string password = "password";
+				new SaltedHash().GetHashAndSaltString(password, out hash, out salt);
+				userRepository.CreateUserAuth(
+					new UserAuth {
+						Id = 1,
+						DisplayName = "JoeUser",
+						Email = "Joe@user.com",
+						UserName = "juser",
+						FirstName = "Joe",
+						LastName = "User",
+						PasswordHash = hash,
+						Salt = salt,
+					}, password);
+				//IHttpResult authenticationRequired = helloService.AuthenticationRequired(); // ????
+			}
+
+			public void SayHello(string text)
+			{
+				HelloService.Post(new Hello() {Name = text});
+			}
+		}
+
+		//Run it!
+		static void Main(string[] args)
+		{
+			var listeningOn = args.Length == 0 ? "http://*:1337/" : args[0];
+			var appHost = new AppHost();
+			appHost.Init();
+			appHost.Start(listeningOn);
+
+			Console.WriteLine("AppHost Created at {0}, listening on {1}", DateTime.Now, listeningOn);
+			while (true) {
+				Console.ReadKey();
+				//appHost.SayHello("Bhavesh");
+			}
+			
+		}
+	}
+}
